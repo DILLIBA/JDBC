9b44d440f70dd2a40f219c1529cb7f514794712a
KDILLIBA <dillipdilliyadhav28@gmail.com>
2023-12-14 14:36:34 +0530

    add two codes

--- src/com/jsp/demo/User.java
+++ src/com/jsp/demo/User.java
@@ -1,5 +1,36 @@
 package com.jsp.demo;
 
 public class User {
+	private int userId;
+	private String name;
+	private String email;
+	private  String password;
+	// command
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
 
 }
